2149ee11df883be7971d8586bb5a78ac49f46c8d
Cleiton Arcanjo <cleiton.arcanjo@outlook.com>
2020-09-20 20:52:11 -0300

    1° versão

---
+++ AppArcanjo/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import {HttpClientModule} from '@angular/common/http'; // fazer requisições http em uma API
+import { UsuarioComponent } from './usuario/usuario.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+      NavComponent,
+      UsuarioComponent,
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: [NavComponent]
+})
+export class AppModule { }
---
+++ AppArcanjo/src/app/usuario/usuario.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.css']
+})
+export class UsuarioComponent implements OnInit {
+
+  usuarios: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    // evento vai ser executado antes do html ficar pronto
+    this.getUsuarios();
+  }
+
+  // tslint:disable-next-line: typedef
+  getUsuarios(){
+    // aqui faço requisições http na API
+    this.usuarios = this.http.get('http://localhost:52246/api/users').subscribe(
+      // tslint:disable-next-line: semicolon
+      response => {this.usuarios = response
+        // tslint:disable-next-line: align
+        console.log(response);
+      },
+      Error => {
+        console.log(Error);
+      },
+    // tslint:disable-next-line: semicolon
+    )
+  }
+
+}
