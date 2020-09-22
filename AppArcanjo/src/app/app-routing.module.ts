d57b3ef324877d2029e9a4ccae9f05eb075d20aa
Cleiton Arcanjo <cleiton.santos@zenatur.com.br>
2020-09-22 17:11:18 -0300

    Ajustando Rotas

--- AppArcanjo/src/app/app-routing.module.ts
+++ AppArcanjo/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
-const routes: Routes = [];
+// configuro as rotas dos meus componentes
+const routes: Routes = [
+  {path: 'app', component: AppComponent},
+  {path: 'users', component: UsuarioComponent},
+  {path: 'nav', component: NavComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
